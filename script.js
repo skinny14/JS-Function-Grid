2012c6f8ca22d4974559a4384395862a0a32649d
Christian Chavez <chris6141.cc.cc@gmail.com>
2017-02-15 21:06:30 +0000

    the face...lol.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //Write your function calls below this line
+setColor(2,2, "black"
+);
+setColor(2,6, "black"
+);
+setColor(4,3, "black"
+);
+setColor(4,4, "black"
+);
+setColor(4,5, "black"
+);
+setColor(5,4, "black"
+);
